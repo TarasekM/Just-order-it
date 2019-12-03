599bae3f4a404b23398619847116311a4751e80d
TarasekM <rezkth@gmail.com>
2019-12-03 20:55:44 +0100

    Add model for Item.

---
+++ models/item.py
@@ -0,0 +1,14 @@
+class Item(object):
+    def __init__(self, name=None, price=None, tags=None, ingredients=None):
+        self.name = name
+        self.price = price
+        self.tags = tags
+        self.ingredients = ingredients
+
+    def get_dict(self):
+        item = dict()
+        item['Name'] = self.name
+        item['Price'] = self.price
+        item['Tags'] = self.tags
+        item['Ingredients'] = self.ingredients
+        return item
