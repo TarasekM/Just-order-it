cd94ea7588817ae7be8a281dc42b45f103c3984d
mateuszliber <mateusz.liber@smcebi.edu.pl>
2020-01-15 22:10:10 +0100

    Added more card products

--- create_menu.py
+++ create_menu.py
@@ -2,11 +2,11 @@ import requests
 import json
 url = "http://localhost:9000/menu"
 
-
+# Main Meals
 pizza1 = {
     "name" : "Pizza Prosciutto",
     "price" : 22.00,
-    "tags" : ["Main Meals"],
+    "tags" : ["Main Meal"],
     "ingredients" : ["prosciutto", "cheese", "tomato souce"]
 }
 response = requests.post(url, pizza1, verify=False)
@@ -14,7 +14,7 @@ response = requests.post(url, pizza1, verify=False)
 pizza2 = {
     "name" : "Pizza Funghi",
     "price" : 20.00,
-    "tags" : ["Main Meals"],
+    "tags" : ["Main Meal"],
     "ingredients" : ["mashrooms", "cheese", "tomato souce"]
 }
 response = requests.post(url, pizza2, verify=False)
@@ -22,7 +22,7 @@ response = requests.post(url, pizza2, verify=False)
 pizza3 = {
     "name" : "Pizza Pepperoni",
     "price" : 22.00,
-    "tags" : ["Main Meals"],
+    "tags" : ["Main Meal"],
     "ingredients" : ["spicy salami", "cheese", "tomato souce"]
 }
 response = requests.post(url, pizza3, verify=False)
@@ -30,7 +30,7 @@ response = requests.post(url, pizza3, verify=False)
 meal1 = {
     "name" : "Devolay",
     "price" : 25.00,
-    "tags" : ["Main Meals"],
+    "tags" : ["Main Meal"],
     "ingredients" : ["pork", "butter", "potatos", "salad"]
 }
 response = requests.post(url, meal1, verify=False)
@@ -38,7 +38,41 @@ response = requests.post(url, meal1, verify=False)
 meal2 = {
     "name" : "Chicken Risotto",
     "price" : 18.00,
-    "tags" : ["Main Meals"],
+    "tags" : ["Main Meal"],
     "ingredients" : ["chicken", "rise", "olive", "vegetables"]
 }
 response = requests.post(url, meal1, verify=False)
+
+# Soups
+soup1 = {
+    "name" : "Tomato Soup",
+    "price" : 10.00,
+    "tags" : ["Soup"],
+    "ingredients" : ["tomatos", "onion", "spices", "rice"]
+}
+response = requests.post(url, soup1, verify=False)
+
+soup2 = {
+    "name" : "Broth Soup",
+    "price" : 10.00,
+    "tags" : ["Soup"],
+    "ingredients" : ["chicken broth", "noodles", "carrot"]
+}
+response = requests.post(url, soup2, verify=False)
+
+#Appetizers
+appetizer1 = {
+    "name" : "Bread with lard",
+    "price" : 15.00,
+    "tags" : ["Appetizer"],
+    "ingredients" : ["bread", "lard", "salt", "pickle"]
+}
+response = requests.post(url, appetizer1, verify=False)
+
+appetizer2 = {
+    "name" : "Stuffed Cucumber",
+    "price" : 13.00,
+    "tags" : ["Appetizer"],
+    "ingredients" : ["cucumber", "vegetables", "spices"]
+}
+response = requests.post(url, appetizer2, verify=False)
