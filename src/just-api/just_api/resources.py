1b98b9084ae99026ec8f6fa050a6e4bf8145d56b
unknown <m.tarasek@brainhint.com>
2020-01-15 09:19:39 +0100

    Add order api

--- src/just-api/just_api/resources.py
+++ src/just-api/just_api/resources.py
@@ -1,3 +1,5 @@
+import datetime
+import json
 from bson.objectid import ObjectId
 from flask import jsonify, make_response, request
 from flask_restful import Api, Resource, reqparse
@@ -8,9 +10,55 @@ api = Api()
 
 item_arg_parser = reqparse.RequestParser()
 item_arg_parser.add_argument(
-    'name', dest='name', type=str, help='Name of the menu item.')
+    'name', dest='name', type=str, help='(str) Name of the menu item.')
 item_arg_parser.add_argument(
-    'price', dest='price', type=int, help='Price of the menu item.')
+    'price', dest='price', type=float, help='(float) Price of the menu item.')
+item_arg_parser.add_argument(
+    'tags', dest='tags', type=str, action='append', help='(list) Tags of menu item.')
+item_arg_parser.add_argument(
+    'ingredients', dest='ingredients', type=str, action='append', help='(list) Ingredients of menu item')
+
+order_arg_parser = reqparse.RequestParser()
+order_arg_parser.add_argument(
+    'order_date', dest='order_date', type=str, help='(str) Date of the order.')
+order_arg_parser.add_argument(
+    'order_pickup_date', dest='order_pickup_date', type=str, help='(str) Date of the pickup order.' )
+order_arg_parser.add_argument(
+    'items', dest='items', type=str, action='append', help='(str) IDs of items in the order')
+order_arg_parser.add_argument(
+    'total_price', dest='total_price', type=float, help='(float) Total price of the order')
+
+
+class Order(Resource):
+
+    def get(self):
+        return make_response(jsonify(list(mongo.db.order.find({}))), 200)
+
+    def post(self):
+        args = order_arg_parser.parse_args()
+        args['order_date'] = datetime.datetime.now()
+        args['order_pickup_date'] = ''
+        inserted_id = mongo.db.order.insert_one(args).inserted_id
+        return make_response(jsonify({'_id': inserted_id}), 201)
+
+
+class SpecificOrder(Resource):
+
+    def get(self, _id):
+        item = mongo.db.order.find_one_or_404({'_id': _id})
+        return make_response(jsonify(item), 200)
+
+    def put(self, _id):
+        args = item_arg_parser.parse_args()
+        mongo.db.order.find_one_or_404({'_id': _id})
+        mongo.db.order.find_one_and_update({"_id": _id},
+                                          {"$set": args})
+        return make_response(jsonify({'_id': _id}), 200)
+
+    def delete(self, _id):
+        mongo.db.order.find_one_or_404({'_id': _id})
+        mongo.db.order.delete_one({'_id': _id})
+        return make_response(jsonify({'_id': _id}), 200)
 
 
 class MenuItem(Resource):
@@ -45,3 +93,5 @@ class Menu(Resource):
 
 api.add_resource(MenuItem, '/menu/<ObjectId:_id>')
 api.add_resource(Menu, '/menu')
+api.add_resource(Order, '/order')
+api.add_resource(SpecificOrder, '/order/<ObjectId:_id>')
